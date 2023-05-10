a45416e4ec258e639866a26fc97daace24641910
Sy <992364879>
2023-05-10 22:03:02 +0800

    commit1

--- GitTest.cpp
+++ GitTest.cpp
@@ -1,14 +1,12 @@
-﻿// GitTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿
+// GitTest.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
 
 int main()
 {
-    std::cout << "Hello World! syyyyyyyyy\n";
-    std::cout<<"test by sy"<<std::endl;
-    std::cout<<"test by sy2"<<std::endl;
-    std::cout<<"test by sy2"<<std::endl;
-    std::cout<<"test by sy3"<<std::endl;
+	std::cout << "11111" << std::endl;
+
 }
 
