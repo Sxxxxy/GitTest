71fdceffae74f198ec29a22199fa3ef5d02ebd8d
Sy <992364879>
2023-05-16 23:55:32 +0800

    add

--- GitTest.cpp
+++ GitTest.cpp
@@ -13,13 +13,9 @@ public:
 
 int main()
 {
-	order o;
-	o.num = 1;
-
-	std::vector<order> v;
-	v.emplace_back(o);
-	o.num = 2;
-	std::cout << o.num<<std::endl;
-	std::cout << v[0].num << std::endl;
+	std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
+	std::cout<<"hello,world"<<std::endl;
 }
 
